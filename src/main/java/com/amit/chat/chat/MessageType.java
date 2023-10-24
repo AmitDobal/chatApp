c5212e2de91506b5f75aaa6944cbfa8627099d5f
Amit Dobal <2019pte5188@mnit.ac.in>
2023-10-24 09:39:56 +0530

    chat app

---
+++ src/main/java/com/amit/chat/chat/MessageType.java
@@ -0,0 +1,5 @@
+package com.amit.chat.chat;
+
+public enum MessageType {
+	CHAT, JOIN, LEAVE
+}
---
+++ src/main/java/com/amit/chat/config/WebSocketEventListener.java
@@ -0,0 +1,30 @@
+package com.amit.chat.config;
+
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.amit.chat.chat.ChatMessage;
+import com.amit.chat.chat.MessageType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+	private final SimpMessageSendingOperations messageTemplate;
+
+	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+		if (username != null) {
+			log.info("User disconnected: {}", username);
+			var chatMessage = ChatMessage.builder().type(MessageType.LEAVE).sender(username).build();
+			messageTemplate.convertAndSend("/topic/public", chatMessage);
+		}
+	}
+}
